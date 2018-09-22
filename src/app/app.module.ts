f685b74e52075f0cc5a371c168c6ac369327e4e3
thrilok209 <thrilok2000@gmail.com>
2018-09-22 19:49:11 +0530

    first testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+
+import { RouterModule } from '@angular/router';
+
+import { routes } from './app.routes';
+import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+import { FooterComponent } from './component/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppBootstrapModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+// import {HeeeComponent} from "./heee/heee.component";
+import {Routes} from "@angular/router"
+export const routes: Routes = [
+  // {
+  //   path: 'sendingBusDetails',
+  //   component:
+  // }
+]
